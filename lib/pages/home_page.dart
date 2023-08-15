12acd6f59fd0d86b74bdf93305d999e40d001795
mcalisadeghi <ali.ke970@gmail.com>
2023-08-15 23:12:06 +0330

    creat search bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/const/constansts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +11,67 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'home page',
-        ),
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 20.0,
+            ),
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 12.0,
+                ),
+                decoration: BoxDecoration(
+                  color: Constants.primaryColor.withOpacity(
+                    0.1,
+                  ),
+                  borderRadius: BorderRadius.circular(
+                    20.0,
+                  ),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.search,
+                      color: Constants.blackColor.withOpacity(
+                        0.6,
+                      ),
+                    ),
+                    const Expanded(
+                      child: Directionality(
+                        textDirection: TextDirection.rtl,
+                        child: TextField(
+                          textAlign: TextAlign.end,
+                          showCursor: false,
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(
+                                right: 5.0,
+                              ),
+                              hintText: 'search',
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none),
+                          style: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.mic,
+                      color: Constants.blackColor.withOpacity(
+                        0.6,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
