7a5431d6b8f0b258e288e8d0745e51c4481b94a6
mcalisadeghi <ali.ke970@gmail.com>
2023-08-19 19:06:27 +0330

    add  litel text

--- lib/pages/DetaiPage.dart
+++ lib/pages/DetaiPage.dart
@@ -17,9 +17,11 @@ class _DetaiPageeState extends State<DetaiPagee> {
   @override
   Widget build(BuildContext context) {
     List<Plant> plantL = Plant.plantList;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: <Widget>[
+          // App bar
           Positioned(
             top: 50.0,
             left: 25.0,
@@ -31,6 +33,7 @@ class _DetaiPageeState extends State<DetaiPagee> {
                   onTap: () {
                     Navigator.pop(context);
                   },
+                  // close button
                   child: Container(
                     height: 50.0,
                     width: 50.0,
@@ -48,6 +51,7 @@ class _DetaiPageeState extends State<DetaiPagee> {
                     ),
                   ),
                 ),
+                // like button
                 Container(
                   height: 50.0,
                   width: 50.0,
@@ -67,6 +71,50 @@ class _DetaiPageeState extends State<DetaiPagee> {
                   ),
                 ),
               ],
+            ),
+          ),
+          Positioned(
+            child: Container(
+              width: size.width * 0.8,
+              height: size.height * 0.8,
+              padding: EdgeInsets.all(
+                20.0,
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 10.0,
+                    right: 0.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'اندازه گیاه ',
+                              style: TextStyle(
+                                color: Constants.blackColor,
+                                fontSize: 10.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'کوچک',
+                              style: TextStyle(
+                                color: Constants.blackColor,
+                                fontSize: 10.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
