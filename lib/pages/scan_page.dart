46952948e33b086ace888c1d99c2ffda95b44942
mcalisadeghi <ali.ke970@gmail.com>
2023-08-20 19:38:34 +0330

    add menue to can page

--- lib/pages/scan_page.dart
+++ lib/pages/scan_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/const/constansts.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -10,11 +11,60 @@ class ScanPage extends StatefulWidget {
 class _ScanPageState extends State<ScanPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'scan page',
-        ),
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          // App bar
+          Positioned(
+            top: 25.0,
+            left: 50.0,
+            right: 50.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  // close button
+                  child: Container(
+                    height: 50.0,
+                    width: 50.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        50.0,
+                      ),
+                      color: Constants.primaryColor.withOpacity(
+                        0.15,
+                      ),
+                    ),
+                    child: Icon(
+                      Icons.close,
+                      color: Constants.primaryColor,
+                    ),
+                  ),
+                ),
+                // share button
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(
+                      50.0,
+                    ),
+                    color: Constants.primaryColor.withOpacity(
+                      0.15,
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.share,
+                    color: Constants.primaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
