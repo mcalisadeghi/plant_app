c817c7998c59ec2da39ba49e94c368fd731c8fa2
mcalisadeghi <ali.ke970@gmail.com>
2023-08-20 23:41:56 +0330

    scan page dosent work

--- lib/pages/scan_page.dart
+++ lib/pages/scan_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/const/constansts.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -9,8 +11,40 @@ class ScanPage extends StatefulWidget {
 }
 
 class _ScanPageState extends State<ScanPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  late QRScanView controller;
+  bool isCameraPermissionGranted = false;
+  Future<void> _qrsnaner() async {
+    var cameraStatus = await Permission.camera.status;
+    if (!cameraStatus.isGranted) {
+      var permissionStatus = await Permission.camera.request();
+      if (permissionStatus.isGranted) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => QRScanView(),
+          ),
+        );
+      }
+    } else {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => QRScanView(),
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _qrsnaner();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -64,7 +98,86 @@ class _ScanPageState extends State<ScanPage> {
               ],
             ),
           ),
+          Positioned(
+            top: 100.0,
+            left: 20.0,
+            right: 20.0,
+            child: SizedBox(
+              width: size.width * 0.8,
+              height: size.height * 0.8,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        _qrsnaner(); // اجازه دسترسی به دوربین چک می‌شود
+                        if (isCameraPermissionGranted) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  QRScanView(), // به صفحه‌ی اسکن کردن منتقل می‌شویم
+                            ),
+                          );
+                        }
+                      },
+                      child: Image.asset(
+                        'assets/images/code-scan.png',
+                        height: 100.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text(
+                      'scan',
+                      style: TextStyle(
+                        color: Constants.primaryColor.withOpacity(
+                          0.8,
+                        ),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class QRScanView extends StatelessWidget {
+  const QRScanView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('اسکنر QR'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            QrImageView(
+              data: 'https://www.example.com', // با داده‌ی واقعی جایگزین کنید
+              version: QrVersions.auto,
+              size: 200.0,
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'کد QR را اسکن کنید',
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
